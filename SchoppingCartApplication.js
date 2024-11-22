818666d4991ff3b94bcf785690bf064f1f702b7c
Joseph <Joseph@gmail.com>
2024-11-22 20:38:07 +0100

    fourthTask_SchoppingCartApplicationfullVersion

--- SchoppingCartApplication.js
+++ SchoppingCartApplication.js
@@ -6,7 +6,9 @@ class Cart{
     addProduct(...listaProduktow){
         this.listaProduktow.push(...listaProduktow)
     }
-
+    removeProduct(nazwa){
+        this.listaProduktow = this.listaProduktow.filter(produkt => produkt.nazwa !== nazwa);
+    }
 
 }
 
@@ -25,4 +27,6 @@ let cart = new Cart()
 cart.addProduct(new Product("woda",2.5,10))
 cart.addProduct(new Product("piwo",2.5,10))
 cart.addProduct(new Product("sok",2.6,10))
+
+cart.removeProduct("piwo")
 console.log(cart.listaProduktow)
